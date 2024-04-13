967b95037153f0d5c9fa81d293361a902843cae7
Dinis Correia <151139018+dinisjcorreia@users.noreply.github.com>
2024-04-13 10:51:54 +0100

    v

--- src/index.php
+++ src/index.php
@@ -1,18 +1,9 @@
-<<<<<<< Updated upstream
-<?php include 'components/header.php';?>
-=======
+
 <?php include 'components/header.php';
 include '../src/include/config.inc.php';?>
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
 
-
-
->>>>>>> Stashed changes
 
 <?php include 'components/feed.php';?>
 
