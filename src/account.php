a72e88e7368381b993c6f95dd38ade4b106a4488
Notoriety <pedrohenriquems2006@gmail.com>
2024-04-13 12:25:12 +0100

    Create account.php

---
+++ src/account.php
@@ -0,0 +1,46 @@
+<?php 
+include 'components/header.php';
+include 'include/config.inc.php';
+ ini_set('display_errors', 1);
+ error_reporting(E_ALL);   
+
+
+
+ if($_SERVER['REQUEST_METHOD'] == 'GET'){ //Para abrir o perfil de outras pessoas
+    if(isset($_GET['user'])){
+        $id_user = $_GET['user'];
+
+    }
+ }else{
+     $id_user = $_SESSION['user_id']; //Abre o nosso
+ }
+
+
+//echo $_SESSION['user_id'];
+ $user = my_query('SELECT * FROM user WHERE id = ' . $_SESSION['user_id']);
+
+// var_dump($user);
+ $posts = my_query('SELECT posts.* FROM posts INNER JOIN user ON user.id = posts.iduser');
+// var_dump($posts);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil user</title>
+</head>
+<body>
+    <h2>Uutilizador: <?php echo $user[0]['username']; ?></h2>
+    <h2>FOTO:</h2>
+    <!--img src="uploads/Perfil/<?php echo $user[0]['foto']; ?>" alt=""-->
+    <h1>POSTS:</h1>
+    <?php
+        for($i = 0; $i < count($posts); $i++){
+            //Qualquer cena para postar foto
+            echo ' <img src="uploads/'.$posts[$i]['foto'].'" alt="">';
+        }
+
+    ?>
+</body>
+<?php include 'components/footer.php';?>
