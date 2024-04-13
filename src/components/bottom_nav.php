266ba771845a693b8901f80999e08c7ac53e49e1
Hugo Almeida <hugo2006almeida2006@gmail.com>
2024-04-13 16:14:53 +0100

    aa

--- src/components/bottom_nav.php
+++ src/components/bottom_nav.php
@@ -8,6 +8,7 @@
 // echo (basename($_SERVER['PHP_SELF']) == "index.php");
 // die();
 ?>
+
 <div class="btm-nav bg-base-100 text-primary">
   <a href="index.php" <?php echo (basename($_SERVER['PHP_SELF']) == "index.php" || basename($_SERVER['PHP_SELF']) == "") ? 'class="active"' : "" ;?>>
     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-home"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
