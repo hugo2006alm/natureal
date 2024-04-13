2429b37f7e315099af653455863847f18f9958c5
BravoGuerreiro-20 <carlos.f.diogo@gmail.com>
2024-04-13 12:29:30 +0100

    Countdown

--- src/components/header.php
+++ src/components/header.php
@@ -9,6 +9,36 @@
             return /iPhone|iPad|iPod/i.test(navigator.userAgent || navigator.vendor || (window.opera && opera.toString() === `[object Opera]`));
         };
     </script>
+    <script>
+        function updateCountdown() {
+            const hoursSpan = document.getElementById('hourspan');
+            const minutesSpan = document.getElementById('minspan');
+            const secondsSpan = document.getElementById('secspan');
+
+            let now = new Date();
+            let eventDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0); // Define a data do evento
+
+            let currentTime = now.getTime();
+            let eventTime = eventDate.getTime();
+
+            let remTime = eventTime - currentTime;
+
+            let sec = Math.floor(remTime / 1000);
+            let min = Math.floor(sec / 60);
+            let hr = Math.floor(min / 60);
+
+            sec = sec % 60;
+            min = min % 60;
+            hr = hr % 24;
+
+            hoursSpan.style = '--value:' + hr;
+            minutesSpan.style = '--value:' + min;
+            secondsSpan.style = '--value:' + sec;
+        }
+
+        // Chama a função updateCountdown a cada segundo
+        setInterval(updateCountdown, 1000);
+    </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NatuReal</title>
     <link rel="stylesheet" href="output.css">
