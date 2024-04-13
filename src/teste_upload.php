f624ce1df34136f3cd610809c81a0f50d802d88f
Enzo Grigorio <enzonascentess@gmail.com>
2024-04-13 14:46:09 +0100

    teste_upload

---
+++ src/teste_upload.php
@@ -0,0 +1,32 @@
+
+    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<form id="upload-form" enctype="multipart/form-data" action="https://www.nyckel.com/v1/functions/vpj20lceipbwcjjb/invoke">
+    <input type="file" name="file" id="file">
+    <input type="submit" value="Upload">
+</form>
+
+<script>
+$(document).ready(function () {
+    $('#upload-form').on('submit', function(e) {
+        e.preventDefault();
+
+        var formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr('action'),
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                console.log(data); 
+            },
+            error: function(data) {
+                console.log('Error:', data);
+            }
+        });
+    });
+});
+</script>
