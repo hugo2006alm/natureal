5f3af544b9cbfffb578e62aeefdffb95e60c964a
Dinis Correia <151139018+dinisjcorreia@users.noreply.github.com>
2024-04-14 15:53:21 +0100

    c

--- src/challenges.php
+++ src/challenges.php
@@ -22,14 +22,18 @@ if ($queryverificar[0]['total']>2){
 <?php 
 if (isset($_GET['erro'])){
     if ($_GET['erro']==1){
-        echo '<div role="alert" class="alert alert-error max-w-xs">
+        echo '<div class="w-screen flex justify-center items-center">
+        <div role="alert" class="alert alert-error max-w-xs">
         <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
         <span>Não encontramos nenhum pássaro na imagem.</span>
+      </div>
       </div>';
     } else  if ($_GET['erro']==2){
-        echo '<div role="alert" class="alert alert-error max-w-xs">
+        echo '<div class="w-screen flex justify-center items-center">
+        <div role="alert" class="alert alert-error max-w-xs">
         <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
         <span>Já atingiu o limite diário de publicações.</span>
+      </div>
       </div>';
     } 
    
