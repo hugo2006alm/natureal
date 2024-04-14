0d5a885acdfbaa4f1124fe22428435c8bec388c0
Hugo Almeida <hugo2006almeida2006@gmail.com>
2024-04-14 15:18:01 +0100

    Auto stash before merge of "main" and "origin/main"

--- src/components/countdown.php
+++ src/components/countdown.php
@@ -1,5 +1,5 @@
 <div class="z-10 fixed bottom-20 w-screen flex justify-center items-center transition-opacity duration-300" id="countdown-bottom">
-    <div class="flex gap-5 bg-base-100 rounded-md p-4 text-primary">
+    <div class="flex gap-5 bg-base-100 rounded-md p-4 text-primary border border-primary">
         <div>
             <span class="countdown font-mono text-3xl">
                 <span id="hourspan1"></span>
